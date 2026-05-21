834dfafb47f244df11c2131fa2873c51e6227e2b
Eva-004 <foredajannateva@gmail.com>
2026-05-21 12:50:08 +0600

    Added loading spinner and not found page

---
+++ src/app/my-ideas/layout.jsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "My Ideas",
+};
+
+export default function MyIdeaLayout({ children }) {
+  return <>{children}</>;
+}
---
+++ src/app/my-interactions/layout.jsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "My Interactions",
+};
+
+export default function MyInteractionsLayout({ children }) {
+  return <>{children}</>;
+}
